890d4e86673c3499edc6f9e7e3683bd55794e756
Matthew Bellis <matthew.bellis@gmail.com>
2012-02-16 12:47:11 -0600

    Modularized the stepping function.

--- pCT/simple_beam_and_phantom/fire_beam.py
+++ pCT/simple_beam_and_phantom/fire_beam.py
@@ -1,7 +1,6 @@
 import numpy as np
 import scipy as sp
 
-from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
 import pct_utilities as pct
@@ -9,89 +8,89 @@ import pct_utilities as pct
 ################################################################################
 def main():
 
-    nprotons = 10000
-
+    nprotons = 1000
     MASS_proton = 938.272
-
     # Is this really the momentum?
-    proton_energy = 500.0  # MeV?
-    prob_of_scatter = 0.05
+    proton_initial_energy = 500.0  # MeV?
+    prob_of_scatter_in_phantom = 0.05
 
     # Location of detector
     detector_x_position = 100.0
 
     time_step = 0.10 # ns
-    nsteps = 100
 
     size_of_phantom = 10 # cm
 
-    plots = []
-
-    fig = plt.figure(figsize=(12,8))
-    #ax = fig.add_subplot(111, projection='3d')
-    ax = fig.add_subplot(111)
-
     pixel_positions = []
 
-    # Move proton
+    total_energy = np.sqrt(MASS_proton*MASS_proton + proton_initial_energy*proton_initial_energy)
+    initial_velocity = (proton_initial_energy/total_energy)*29.97 # Gives us velocity in cm/ns
+
+    # A bit of plotting declarations.
+    fig = plt.figure(figsize=(12,8))
+    ax = fig.add_subplot(111)
+
     x,y,z = 0.0,0.0,0.0
-    for n in xrange(nprotons):
-
-        total_energy = np.sqrt(MASS_proton*MASS_proton + proton_energy*proton_energy)
-
-        initial_velocity = (proton_energy/total_energy)*29.97 # Gives us velocity in cm/ns
+    n=0
+    while n<nprotons:
 
         vel = np.array([initial_velocity,0.0,0.0]) # velocity of the proton
         pos = np.array([0.0,0.0,0.0]) # position of proton
-
         scattered = False
-
+        prob_of_scatter=prob_of_scatter_in_phantom 
         step_positions = [[],[],[]]
-        # Keep stepping through the simulation until the proton
-        # reaches the detector.
+
+        # Propagate the proton to the detector.
         while pos[0]<detector_x_position:
 
-            pos += vel*time_step
+            # Make sure we're still inside the phantom.
+            if pos[0]>size_of_phantom: 
+                prob_of_scatter=-999 # Won't scatter anymore because we are
+                                     # outside of the phantom.
+                scattered = True
 
-            step_positions[0].append(pos[0])
-            step_positions[1].append(pos[1])
-            step_positions[2].append(pos[2])
+            pos,vel,scattered = pct.take_a_step_in_the_phantom(pos,vel, \
+                    time_step,prob_of_scatter,scattered)
 
-            #print pos
+            # Grab the step positions for plotting later.
+            for j in xrange(3):
+                step_positions[j].append(pos[j])
 
-            # See if we hard scatter once.
-            if not scattered and pos[0]<size_of_phantom:
-
-                # Need to change this so probability of interacting
-                # (scattering) is proportional to how much material
-                # it has traversed in the phantom.
-
-                if np.random.rand()<prob_of_scatter:
-                    #vel[1] += 0.1*np.random.rand()
-                    vel = pct.scatter_hard_Gaussian(vel)
-                    scattered = True
-
-        #print pos[1]
-            
+        ########################################################################
+        # Finished stepping one proton
+        ########################################################################
+        
+        ########################################################################
+        # Plot the scattering info for that one track.
+        ########################################################################
         
         x = step_positions[0]
         y = step_positions[1]
         z = step_positions[2]
 
-        #color = (0.0+n*0.009,0.9-0.005*n,0.3+0.005*n)
-        color = 'red'
-        #ax.plot(list(x),list(y),list(z), c=color)
+        #color = 'red'
+        increment = 1.0/nprotons
+        color = (0.0+n*increment,0.0+n*increment,0.0+n*increment)
         if np.abs(y[-1])>0.1: # Cut out tracks that have small scattering angle.
             ax.plot(x,y,c=color)
             pixel_positions.append(pos[1])
+            if n%100==0:
+                print n
 
+            n += 1
+
+
+    ############################################################################
+    # Plot the information.
+    ############################################################################
     ax.set_xlabel('x-axis')
     ax.set_ylabel('y-axis')
-    #ax.set_zlabel('z-axis')
 
     fig1 = plt.figure(figsize=(6,6))
     ax1 = fig1.add_subplot(111)
 
+    print len(pixel_positions)
+    print np.std(pixel_positions)
     ax1.hist(pixel_positions,bins=25)
 
 
--- pCT/simple_beam_and_phantom/pct_utilities.py
+++ pCT/simple_beam_and_phantom/pct_utilities.py
@@ -1,7 +1,30 @@
 import numpy as np
 
 ################################################################################
+def take_a_step_in_the_phantom(pos,vel,time_step,prob_of_scatter,scattered=False):
+
+    # First move
+    pos += vel*time_step
+
+    if not scattered:
+
+        # Check to see if we've scattered, and if so, 
+        # update the new velocity.
+        #
+        # Need to change this so probability of interacting
+        # (scattering) is proportional to how much material
+        # it has traversed in the phantom.
+        # 
+        if np.random.rand()<prob_of_scatter:
+            vel = scatter_hard_Gaussian(vel)
+            scattered = True
+
+    return pos,vel,scattered
+
+################################################################################
 def scatter_hard_Gaussian(vel):
+
+    # Not yet worrying about energy loss
 
     # Scatter the particle around a Gaussian distribution.
     # Can make it more of a ``hard" scatter by changing the sigma of the
